ce3476b5cea699d3b15d4bd495c9dcaec3f2d840
Federico Rey <freycasg@users.noreply.github.com>
2018-08-21 21:22:11 +0200

    1

--- 14CLASEOBJECT/ClaseObject/src/claseobject/ClaseObject.java
+++ 14CLASEOBJECT/ClaseObject/src/claseobject/ClaseObject.java
@@ -1,14 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package claseobject;
 
 /**
  *
- * @author elbfed1
+ * @author frey
  */
+public class Empleado {
+    private String nombre;
+    private double sueldo;
+    
+    Empleado(String nombre, double sueldo){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public double getSueldo(){
+        return sueldo;
+    }
+    
+    public void setSueldo(double sueldo){
+        this.sueldo = sueldo;
+    }
+    
+    @Override
+    public String toString(){
+        return "Empleado{" + "nombre=" + nombre + ", sueldo=" + sueldo + '}';
+    }
+    
+    @Override
+    public booblean equals(Object obj){
+        if (obj==null){
+            return false;
+        }
+        if (obj instanceof Empleado){
+            Empleado emp = (Empleado) obj;
+            if (nombre.equals(emp.nombre)&&)Do
+        }
+    }
+    
+}
 public class ClaseObject {
 
     /**
